e81de213668c8e0dc2a4c8cc3cf515cf6bc83ff6
fow54 <victor_abresol@hotmail.fr>
2019-02-27 00:20:37 +0100

    test import

---
+++ Assets/Scripts/controller.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class controller : MonoBehaviour {
+
+
+    public float speed = 5;
+    public int hauteur = 0;
+
+    private Rigidbody rb;
+
+    public Transform myChildObject;
+    public Transform myParentObject;
+    public bool detachChild = false;
+
+
+    void Start() {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void FixedUpdate() {
+        if (Input.GetKey(KeyCode.Q)) {
+            detachChild = false;
+        }
+        if (Input.GetKey(KeyCode.W)) {
+            detachChild = true;
+        }
+        if (detachChild == true) {
+            myChildObject.parent = null;
+        }
+        else {
+            myChildObject.transform.parent = myParentObject.transform;
+            myChildObject.transform.localPosition = new Vector3(0, 0, 0);
+            myChildObject.transform.localRotation = new Quaternion(0, 0, 0,0);
+        }
+        hauteur = 0;
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+        if (Input.GetKey(KeyCode.X)) {
+            hauteur = -1;
+        }
+        if (Input.GetKey(KeyCode.Z)) {
+            hauteur = 1;
+        }
+        Vector3 movement = new Vector3(moveHorizontal, hauteur, moveVertical);
+        transform.Translate(movement * Time.deltaTime * speed);
+    }
+}
---
+++ Assets/Scripts/translator.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class translator : MonoBehaviour {
+        public float speed = 1;
+        public float lenght = 100;
+        private float compteur = 0;
+        public Vector3 direction;
+        // Start is called before the first frame update
+        void Start() {
+
+        }
+
+        // Update is called once per frame
+        void Update() {
+            compteur += 1;
+            if (compteur > 10 * lenght) { compteur = 0; }
+            if (compteur > 10 * lenght / 2) { transform.Translate(direction * Time.deltaTime * speed); }
+            if (compteur < 10 * lenght / 2) { transform.Translate(-direction * Time.deltaTime * speed); }
+        }
+}
